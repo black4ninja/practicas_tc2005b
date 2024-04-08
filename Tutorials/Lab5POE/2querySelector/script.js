2615f9ab1dc01c1cbf7db5300af7ef7d7df8582c
Alejandro Fernández <alfervil@meeplab.com>
2024-04-08 12:42:58 -0600

    fix: Lab 6 versiones

--- Tutorials/Lab5POE/2querySelector/script.js
+++
@@ -1,5 +0,0 @@
-const h1Element = document.querySelector("h1");
-console.log(h1Element);
-
-const list = document.querySelector(".list");
-console.log(list);
--- Tutorials/Lab5POE/3querySelectorAll/script.js
+++
@@ -1,8 +0,0 @@
-const listItems = document.querySelectorAll("ul > li");
-console.log(listItems); 
-
-const listaDeElementos = document.querySelectorAll("ul > li");
-
-listaDeElementos.forEach((item) => {
-  console.log(item);
-});
